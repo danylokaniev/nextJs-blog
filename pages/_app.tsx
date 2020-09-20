16a7b728222dd98300299de30b8ff539e7a793ae
Danylo Kanievskyi <danylokaniev@gmail.com>
2020-09-20 11:02:39 +0300

    add _app typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import NextNprogress from 'nextjs-progressbar';
+import type { AppProps } from 'next/app';
 
-
-export default function App({Component, pageProps}) {
-
-    return (<>
-        <NextNprogress color="#29D" startPosition="0.3" stopDelayMs="600" height="5"/>
-        <Component {...pageProps} />
-        <style jsx global>{`
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NextNprogress color="#29D" startPosition="0.3" stopDelayMs="600" height="5" />
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
          html {
            overflow-y: scroll;
          }
         `}
-        </style>
-    </>);
+      </style>
+    </>
+  );
 }
